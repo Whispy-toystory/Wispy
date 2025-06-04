1728dc8320a7fbd2e3c8280acc685b064b879e53
0gonge <Challengef0802@gmail.com>
2025-06-04 18:00:13 +0900

    FEAT: expo-video 설치 및 연결 - #22

--- Wispy/screens/MagicalScreen.js
+++ Wispy/screens/MagicalScreen.js
@@ -1,44 +1,66 @@
-import React, { useState } from 'react';
-import {
-    View,
-    Text,
-    StyleSheet,
-    Platform
-} from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { VideoView, useVideoPlayer } from 'expo-video';
 import Colors from '../constants/colors';
 import Fonts from '../constants/fonts';
 
-function MagicalScreen() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.mainText}> Something magical{'\n'}is about to happen~ </Text>
-            <Text style={styles.mainText}> 
-                <Text style={styles.subText}> Good luck </Text>on your{'\n'}Wispy adventure!
-            </Text>
+const MagicalScreen = () => {
+  const player = useVideoPlayer(require('../assets/video/arrived.mov'), player => {
+    player.loop = false;
+    player.play();
+  });
 
-        </View>
-    );
-}
+  return (
+    <View style={styles.container}>
+      <View style={styles.textWrapper}>
+        <Text style={styles.mainText}>
+          Something magical{'\n'}is about to happen~
+        </Text>
+        <Text style={styles.mainText}>
+          <Text style={styles.subText}>Good luck </Text>
+          on your{'\n'}Wispy adventure!
+        </Text>
+      </View>
+
+      <VideoView
+        style={styles.video}
+        player={player}
+        allowsFullscreen
+        allowsPictureInPicture
+        contentFit="cover"
+      />
+    </View>
+  );
+};
 
 export default MagicalScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'black',
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingHorizontal: 24,
-    },
-    mainText: {
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  textWrapper: {
+    alignItems: 'center',
+    paddingHorizontal: 24,
+  },
+  mainText: {
     textAlign: 'center',
     color: Colors.wispyWhite,
     fontSize: 25,
     lineHeight: 40,
     fontFamily: Fonts.suitHeavy,
+    marginBottom: 10,
   },
-    subText: {
+  subText: {
     color: Colors.wispyButtonYellow,
     fontFamily: Fonts.suitHeavy,
+  },
+  video: {
+    width: 270,
+    height: 405,
+    borderRadius: 16,
   },
 });
